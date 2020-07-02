b67016f008decce7a22533618816324da3f7d0f9
Gustavo Ferreira <gdragnell@live.com>
2020-07-02 11:18:02 -0300

    Boletim

---
+++ Classe3/Nota.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace Classe3
+{
+    class Nota
+    {
+        public string Nome;
+        public double x, y, z;
+        
+
+        public double Media()
+        {
+             return x + y + z;
+        }
+        public bool Aprovado()
+        {
+            if(Media() >= 60.0)
+            {
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double Restante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - Media();
+            }
+        }
+        
+    }
+}
---
+++ Classe3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Classe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nota N = new Nota();
+            int i;
+
+            Console.WriteLine("Nome do Aluno:");
+            N.Nome = Console.ReadLine();
+            Console.WriteLine("Deigite as três notas do aluno:");
+            N.x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            N.y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            N.z = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Nota Final = " + N.Media().ToString("F2",CultureInfo.InvariantCulture));
+            if (N.Aprovado())
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltam =" + N.Restante().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
